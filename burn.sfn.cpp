c210e37960a76dd241e679f0460a1e370eda6986
Denis Carriere <carriere.denis@gmail.com>
2025-01-08 11:04:21 -0500

    add eosoi

--- burn.sfn.cpp
+++ burn.sfn.cpp
@@ -1,7 +1,7 @@
 #include <eosio/eosio.hpp>
 
-const name COLLECTION_NAME = "shufan.free"_n;
-const name REFUND_BYTES_FROM = "burn.sfn"_n;
+const eosio::name COLLECTION_NAME = "shufan.free"_n;
+const eosio::name REFUND_BYTES_FROM = "burn.sfn"_n;
 const int64_t REFUND_BYTES = 151; // 151 bytes
 const int64_t TEMPLATE_ID = 11168;
 
